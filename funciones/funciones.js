92879fe785960f6ee154c18815c431c597410fa6
Raquel <rac.alarcon13@gmail.com>
2023-01-29 04:34:57 +0100

    faltaban los comentarios del archivo js

--- funciones/funciones.js
+++ funciones/funciones.js
@@ -2,9 +2,9 @@ const imagenes = document.querySelectorAll(".galeria a");
 const modal = document.querySelector(".modal");
 const imgModal = document.querySelector(".modal img");
 const flechas = document.querySelectorAll(".modal a");
-//variables de estado
-let rutasImg = []; // esto guarda las urls de las imagenes
-let imagenActiva = 13; //numero que representa un indice de ruta img
+
+let rutasImg = []; 
+let imagenActiva = 13; 
 
 
 //abrir la modal
@@ -19,7 +19,6 @@ imagenes.forEach((imagen,indice)=> {
 
 });
 
-//al hacerle click en cualquier parte se cierre
 modal.addEventListener("click", function(){
     this.classList.remove("visible");
 });
@@ -27,10 +26,9 @@ for(let i = 0; i < flechas.length; i++){
     flechas[i].addEventListener("click", function(evento){
         evento.preventDefault();
         evento.stopPropagation();
-        if( i == 0){// i representa la direcion, 0 atras 1 adelamte.
-            //atras
+        if( i == 0){
             imagenActiva = imagenActiva > 0 ? imagenActiva -1 : rutasImg.length -1;
-        }else{//alante
+        }else{
             imagenActiva = imagenActiva < rutasImg.length -1 ? imagenActiva + 1 :0;
         }
         imgModal.setAttribute("src",rutasImg[imagenActiva]);
